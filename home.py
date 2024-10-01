0947080200bb6f4441e6d22ecc6515c9c96a77fe
AdhithyaGN <adhithyagnair97@gmail.com>
2024-10-01 20:11:43 +0530

    major commit

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Main App - Home")
-st.write("Welcome to the Home page of your Streamlit multi-page app!")
+st.title("Reverse DCF")
+st.write("This site provides interactive tools to valuate and analyze stocks through Reverse DCF model. Check the left page pane for more")
